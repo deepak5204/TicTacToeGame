a3bb4b1319f763f2e5a121c3f71682dd318713ac
deepak.kumar2 <deepak.kumar2@batterysmart.in>
2025-05-29 01:52:16 +0530

    decide player representation and made game state

--- app/src/main/java/com/example/tictactoepro/MainActivity.kt
+++ app/src/main/java/com/example/tictactoepro/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tictactoepro
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,7 +15,26 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.tictactoepro.ui.theme.TicTacToeProTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    // player representation
+    // 0 - X
+    // 1 - O
+
+    val activePlayer = 0
+    val gameState = mutableListOf(2, 2, 2, 2, 2, 2, 2, 2, 2)
+    // state meanings
+    // 0 - X
+    // 1 - O
+    // 2 - Null
+
+
+
+    fun playerTap(view: View) {
+        // Your logic here
+    }
+
+    @Override
+    protected  override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Use XML layout instead of Compose
         setContentView(R.layout.activity_main)
