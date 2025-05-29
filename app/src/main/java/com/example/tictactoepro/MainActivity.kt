2722541bbb131d00b8a6d3b48a646426a907ebc9
deepak.kumar2 <deepak.kumar2@batterysmart.in>
2025-05-30 01:24:05 +0530

    git commit -m "show the winning player at status bar and on next tap click reset the game start again"

--- app/src/main/java/com/example/tictactoepro/MainActivity.kt
+++ app/src/main/java/com/example/tictactoepro/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tictactoepro
 import android.media.Image
 import android.os.Bundle
 import android.util.Log
+import android.util.Log.i
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,12 +15,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import com.example.tictactoepro.ui.theme.TicTacToeProTheme
 
 class MainActivity : ComponentActivity() {
+
+    val isGameActive = mutableStateOf(false)
 
     // player representation
     // 0 - X
@@ -49,6 +53,10 @@ class MainActivity : ComponentActivity() {
         val img = view as ImageView
         val tappedImage = img.tag.toString().toInt()
 
+        if(!isGameActive.value){
+            restartGame()
+        }
+
         if (gameState[tappedImage] == 2) {
             gameState[tappedImage] = activePlayer
             img.translationY = -1000f
@@ -66,8 +74,50 @@ class MainActivity : ComponentActivity() {
 
             img.animate().translationYBy(1000f).duration = 300
         }
+
+        // check winning position
+        for(arr in winningPositions){
+            if(gameState[arr[0]] == gameState[arr[1]] && gameState[arr[1]] == gameState[arr[2]] && gameState[arr[0]] != 2){
+                // somebody won -> find who
+                var winner = ""
+                if(gameState[arr[0]] == 0){
+                    winner = "X's has won "
+                }
+                else {
+                    winner = "O's has won "
+                }
+
+                //update the status bar for winner position
+                val status: TextView = findViewById(R.id.textView)
+                status.text = winner
+                isGameActive.value = false
+            }
+        }
     }
 
+
+    fun restartGame(){
+        isGameActive.value = true
+        activePlayer = 0
+        val status: TextView = findViewById(R.id.textView)
+        status.text = "X's Turn - Tap to play"
+        for(i in gameState.indices){
+            gameState[i] = 2
+
+        }
+
+        findViewById<ImageView>(R.id.imageView0).setImageDrawable(null)
+        findViewById<ImageView>(R.id.imageView1).setImageDrawable(null)
+        findViewById<ImageView>(R.id.imageView2).setImageDrawable(null)
+        findViewById<ImageView>(R.id.imageView3).setImageDrawable(null)
+        findViewById<ImageView>(R.id.imageView4).setImageDrawable(null)
+        findViewById<ImageView>(R.id.imageView5).setImageDrawable(null)
+        findViewById<ImageView>(R.id.imageView6).setImageDrawable(null)
+        findViewById<ImageView>(R.id.imageView7).setImageDrawable(null)
+        findViewById<ImageView>(R.id.imageView8).setImageDrawable(null)
+
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
